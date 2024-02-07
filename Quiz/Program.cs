1f61bd6d654d94be7b36dfd154fcbf23bbbff76d
Jaume <a2308741@institutmontilivi.cat>
2024-02-07 20:14:35 +0100

    MenuPrincipal

--- Quiz/Program.cs
+++ Quiz/Program.cs
@@ -4,7 +4,116 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ConsoleKeyInfo tecla;
+            do
+            {
+                Console.Clear();
+                ShowOptions();
+                tecla = Console.ReadKey();
+                Console.Clear();
+                switch (tecla.Key)
+                {
+                    case ConsoleKey.D1:
+                    case ConsoleKey.NumPad1:
+                        HistoryQuiz();
+                        break;
+                    case ConsoleKey.D2:
+                    case ConsoleKey.NumPad2:
+                        FoodQuiz();
+                        break;
+                    case ConsoleKey.D3:
+                    case ConsoleKey.NumPad3:
+                        AstrologyQuiz();
+                        break;
+                    case ConsoleKey.D4:
+                    case ConsoleKey.NumPad4:
+                        EntertainmentQuiz();
+                        break;
+                    case ConsoleKey.D5:
+                    case ConsoleKey.NumPad5:
+                        SportsQuiz();
+                        break;
+                    case ConsoleKey.D6:
+                    case ConsoleKey.NumPad6:
+                        ITQuiz();
+                        break;
+                    case ConsoleKey.D7:
+                    case ConsoleKey.NumPad7:
+                        MathQuiz();
+                        break;
+                    case ConsoleKey.D8:
+                    case ConsoleKey.NumPad8:
+                        GeoraphyQuiz();
+                        break;
+                    case ConsoleKey.D9:
+                    case ConsoleKey.NumPad9:
+                        MusicQuiz();
+                        break;
+                    case ConsoleKey.D0:
+                    case ConsoleKey.NumPad0:
+                        MsgNextScreen("PRESS ANY KEY 2 EXIT");
+                        break;
+                    default:
+                        MsgNextScreen("Error. Prem una tecla per tornar al menú...");
+                        break;
+                }
+            } while (tecla.Key != ConsoleKey.D0 && tecla.Key != ConsoleKey.NumPad0);
+        }
+
+        public static void ShowOptions()
+        {
+            Console.Clear();
+            Console.WriteLine("1 - History");
+            Console.WriteLine("2 - Food");
+            Console.WriteLine("3 - Astrology");
+            Console.WriteLine("4 - Entertainment");
+            Console.WriteLine("5 - Sports");
+            Console.WriteLine("6 - IT");
+            Console.WriteLine("7 - Math");
+            Console.WriteLine("8 - Georaphy");
+            Console.WriteLine("9 - Music");
+            Console.WriteLine("0 - EXIT");
+        }
+        public static void MsgNextScreen(string msg)
+        {
+            Console.WriteLine(msg);
+            Console.ReadKey();
+        }
+        private static void HistoryQuiz()
+        {
+            throw new NotImplementedException();
+        }
+        private static void FoodQuiz()
+        {
+            throw new NotImplementedException();
+        }
+        private static void AstrologyQuiz()
+        {
+            throw new NotImplementedException();
+        }
+        private static void EntertainmentQuiz()
+        {
+            throw new NotImplementedException();
+        }
+        private static void SportsQuiz()
+        {
+            throw new NotImplementedException();
+        }
+        private static void ITQuiz()
+        {
+            throw new NotImplementedException();
+        }
+        private static void MathQuiz()
+        {
+            throw new NotImplementedException();
+        }
+        private static void GeoraphyQuiz()
+        {
+            throw new NotImplementedException();
+        }
+        private static void MusicQuiz()
+        {
+            throw new NotImplementedException();
         }
     }
 }
