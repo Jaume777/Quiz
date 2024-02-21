037284e7b9c4fe7cf1034c8bf18fafd56a35735c
Glen Morton <mr_mueller3@hotmail.com>
2024-02-21 19:59:31 +0100

    Remove wrong program

--- Quiz/Program.cs
+++ Quiz/Program.cs
@@ -23,7 +23,7 @@
                         break;
                     case ConsoleKey.D3:
                     case ConsoleKey.NumPad3:
-                        AstrologyQuiz();
+                        AstronomyQuiz();
                         break;
                     case ConsoleKey.D4:
                     case ConsoleKey.NumPad4:
@@ -65,7 +65,7 @@
             Console.Clear();
             Console.WriteLine("1 - History");
             Console.WriteLine("2 - Food");
-            Console.WriteLine("3 - Astrology");
+            Console.WriteLine("3 - Astronomy");
             Console.WriteLine("4 - Entertainment");
             Console.WriteLine("5 - Sports");
             Console.WriteLine("6 - IT");
@@ -87,7 +87,7 @@
         {
             throw new NotImplementedException();
         }
-        private static void AstrologyQuiz()
+        private static void AstronomyQuiz()
         {
             throw new NotImplementedException();
         }
@@ -105,7 +105,181 @@
         }
         private static void MathQuiz()
         {
-            throw new NotImplementedException();
+            
+            try
+            {
+                string answer;
+                int correctCount = 0;
+
+                Console.Clear();
+                Console.WriteLine("Let's learn some Maths!! :D");
+                Thread.Sleep(1000);
+                Console.WriteLine();
+                Console.WriteLine("Press 'Y' key to continue...");
+                while (Console.ReadKey().Key != ConsoleKey.Y) ;
+                Console.Clear();
+
+                //Question 1
+                Console.WriteLine("Question 1:");
+                Console.WriteLine("-----------");
+                Console.WriteLine();
+                Thread.Sleep(1000);
+                Console.WriteLine("What is the square root of 49?");
+                Console.WriteLine("a) \t 4");
+                Console.WriteLine("b) \t 9");
+                Console.WriteLine("c) \t 7");
+                Console.WriteLine("d) \t 12");
+
+                answer = Console.ReadLine();
+
+                if ((answer == "c") || (answer == "7"))
+                {
+                    Console.WriteLine("Well done!! Your answer is correct :)");
+                    correctCount++;
+                }
+                else Console.WriteLine("Your answer is incorrect");
+
+                Thread.Sleep(1000);
+                Console.WriteLine();
+                Console.WriteLine("Press 'Y' key to continue...");
+                while (Console.ReadKey().Key != ConsoleKey.Y) ;
+                Console.Clear();
+
+                //Question 2
+                Console.WriteLine("Question 2:");
+                Console.WriteLine("-----------");
+                Thread.Sleep(1200);
+                Console.WriteLine();
+                Console.WriteLine("What is 12 squared?");
+                Console.WriteLine("a) \t 100");
+                Console.WriteLine("b) \t 1200");
+                Console.WriteLine("c) \t 512");
+                Console.WriteLine("d) \t 144");
+
+                answer = Console.ReadLine();
+
+                if ((answer == "d") || (answer == "144"))
+                {
+                    Console.WriteLine("Well done!! Your answer is correct :)");
+                    correctCount++;
+                }
+                else Console.WriteLine("Your answer is incorrect");
+
+                Thread.Sleep(1000);
+                Console.WriteLine();
+                Console.WriteLine("Press 'Y' key to continue...");
+                while (Console.ReadKey().Key != ConsoleKey.Y) ;
+                Console.Clear();
+
+                //Question 3
+                Console.WriteLine("Question 3:");
+                Console.WriteLine("-----------");
+                Thread.Sleep(1000);
+                Console.WriteLine();
+                Console.WriteLine("What is 500 divided by 4?");
+                Console.WriteLine("a) \t 150");
+                Console.WriteLine("b) \t 120");
+                Console.WriteLine("c) \t 140");
+                Console.WriteLine("d) \t 125");
+
+                answer = Console.ReadLine();
+
+                if ((answer == "d") || (answer == "125"))
+                {
+                    Console.WriteLine("Well done!! Your answer is correct :)");
+                    correctCount++;
+                }
+                else Console.WriteLine("Your answer is incorrect");
+
+                Thread.Sleep(1000);
+                Console.WriteLine();
+                Console.WriteLine("Press 'Y' key to continue...");
+                while (Console.ReadKey().Key != ConsoleKey.Y) ;
+                Console.Clear();
+
+                //Question 4
+                Console.WriteLine("Question 4:");
+                Console.WriteLine("-----------");
+                Thread.Sleep(1000);
+                Console.WriteLine();
+                Console.WriteLine("What is 64 x 12?");
+                Console.WriteLine("a) \t 1044");
+                Console.WriteLine("b) \t 515");
+                Console.WriteLine("c) \t 672");
+                Console.WriteLine("d) \t 763");
+
+                answer = Console.ReadLine();
+
+                if ((answer == "c") || (answer == "672"))
+                {
+                    Console.WriteLine("Well done!! Your answer is correct :)");
+                    correctCount++;
+                }
+                else Console.WriteLine("Your answer is incorrect");
+
+                Thread.Sleep(1000);
+                Console.WriteLine();
+                Console.WriteLine("Press 'Y' key to continue...");
+                while (Console.ReadKey().Key != ConsoleKey.Y) ;
+                Console.Clear();
+
+                //Question 5
+                Console.WriteLine("Question 5:");
+                Console.WriteLine("-----------");
+                Thread.Sleep(1000);
+                Console.WriteLine();
+                Console.WriteLine("If a rectangle has a length of 7 meters and a width of 3 meters, what is its area?");
+                Console.WriteLine("a) \t 21");
+                Console.WriteLine("b) \t 30");
+                Console.WriteLine("c) \t 51");
+                Console.WriteLine("d) \t 10");
+
+                answer = Console.ReadLine();
+
+                if ((answer == "a") || (answer == "21"))
+                {
+                    Console.WriteLine("Well done!! Your answer is correct :)");
+                    correctCount++;
+                }
+                else Console.WriteLine("Your answer is incorrect");
+
+                Thread.Sleep(1000);
+                Console.WriteLine();
+                Console.WriteLine("Press 'Y' key to continue...");
+                while (Console.ReadKey().Key != ConsoleKey.Y) ;
+                Console.Clear();
+
+                //Result
+                Console.Write("Your final result is... ");
+                Thread.Sleep(1000);
+                if (correctCount >= 3)
+                {
+                    Console.Write($"{correctCount} out of 5. ");
+                    Thread.Sleep(1000);
+                    Console.WriteLine("Well done!!");
+                    Thread.Sleep(1000);
+                }
+                else
+                {
+                    Console.Write($"{correctCount} out of 5 :( ");
+                    Thread.Sleep(1000);
+                    Console.WriteLine("Better luck next time!");
+                    Thread.Sleep(1000);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                MsgNextScreen("Press any key to return to the main menu...");
+            }
+
+            
+            //Console.WriteLine();
+            //Console.WriteLine("Press any key to return to the main menu...");
+            //Console.ReadKey();
         }
         private static void GeoraphyQuiz()
         {
